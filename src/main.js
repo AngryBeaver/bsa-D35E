1daf3485094512509c298f0e56145155003e982f
AngryBeaver <github@verplanmich.net>
2024-08-29 16:31:33 +0200

    add modulare Tests

--- src/main.js
+++ src/main.js
@@ -3,3 +3,8 @@ import {D35E} from "./D35E.js";
 Hooks.on("beavers-system-interface.init", async function(){
     beaversSystemInterface.register(new D35E());
 });
+
+Hooks.on("beavers-system-interface.ready", async function(){
+    import("./SkillTest.js")
+    import("./AbilityTest.js")
+});
